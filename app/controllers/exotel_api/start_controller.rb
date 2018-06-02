877c62ecbf8b26247c6cc828dc6025af21e29c5a
Aashutosh Chaudhary <aashutosh@unnatihelpers.com>
2018-06-02 07:48:06 +0530

    rails 5

--- app/controllers/exotel_api/start_controller.rb
+++ app/controllers/exotel_api/start_controller.rb
@@ -1,6 +1,6 @@
 module ExotelApi
   class StartController < ApplicationController
-		skip_before_action  :verify_authenticity_token
+		# skip_before_action  :verify_authenticity_token
     def direction
       if params[:Direction] == 'incoming'
         eval(ExotelApi.inbound_query)
